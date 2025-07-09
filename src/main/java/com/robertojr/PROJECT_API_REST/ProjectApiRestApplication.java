18678788d822f31f3dd6eaee9596347ac2d1a0e6
Roberto Jr <113848806+spinolajunior@users.noreply.github.com>
2025-07-08 22:29:26 -0300

    First commit , project is begin

---
+++ src/main/java/com/robertojr/PROJECT_API_REST/ProjectApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.robertojr.PROJECT_API_REST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robertojr/PROJECT_API_REST/ProjectApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertojr.PROJECT_API_REST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
